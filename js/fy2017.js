d47499c772608c1d34f1237a44c2dd5b26720f8a
pengteling <pengteling@gmail.com>
2017-04-01 15:15:53 +0800

    good sass

--- js/fy2017.js
+++ js/fy2017.js
@@ -18,13 +18,31 @@ require("./pJqueryAppearAnimateCSS3/jac.js"); //动画
 
 
 $(function(){
-	$(".im6 li").hover(function(){
-		$(this).find(".layer").stop(true,true).animate({"top":"0"},300);
-	},function(){
-		$(this).find(".layer").stop(true,true).animate({"top":"100%"},300);
+	$(".im7 .im7-body table tr#hid").nextAll("tr").hide();
+	$(".im7 .more").click(function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		$(".im7 .im7-body table tr#hid").nextAll("tr").show();
 	});
+	$(".im-form .ipt-btn").click(function(e){
+		e.preventDefault();
+		e.stopPropagation();
+		var isgood =true;
+		var re = /^1\d{10}$/ ;
+		if($(".im-form .ipt-txt").eq(0).val()==""){
+			alert("请输入您的姓名！");
+			isgood =false;
+		}else if(!re.test( $(".im-form .ipt-txt").eq(1).val())){
+			alert("您输入的手机号码格式不对哦");
+			isgood =false ;
+		}
+		if(isgood){
+			$(".im-form form").submit();
+		}
 
-	$(".slider").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"leftLoop",autoPlay:true,scroll:4,vis:4});
+
+	})
+	
 
 })
 
